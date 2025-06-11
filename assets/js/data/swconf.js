3ffd8ccbbca923cbe033a7bf4b5243a048aad072
cicerojr10 <cicerojr10@users.noreply.github.com>
2025-06-11 02:28:08 +0000

    deploy: a0f9cab0469a95226268906b00dbdb5b3642d5a3

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749608539',resources: [
+    cacheName: 'chirpy-1749608887',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
