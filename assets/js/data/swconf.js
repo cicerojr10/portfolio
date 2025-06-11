b4766feb39536f6028388b90751985194cecf6c0
cicerojr10 <cicerojr10@users.noreply.github.com>
2025-06-11 02:05:19 +0000

    deploy: 8d5a50725143049695d7a70fba9845411b156925

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,30 @@
-const swconf = { purge: true };
+const swconf = {
+  
+    cacheName: 'chirpy-1749607518',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/projects/',
+      
+        '/skills/',
+      
+        '/contact/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
